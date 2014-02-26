3faa54501172114a036a30f72064028b9fc7c917
rick carlino <rick.carlino@gmail.com>
2014-02-25 20:07:22 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var titlexyzApp = angular.module('titlexyzApp', ['ngRoute']);
+angular.module('titlexyzApp.controllers', []);
+
+titlexyzApp.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/menu1', {
+        templateUrl: 'partials/menu1.html',
+        controller: 'menu1Ctrl'
+    }).
+    when('/main', {
+        templateUrl: 'partials/main.html',
+        controller: 'mainCtrl'
+    }).
+    otherwise({
+      redirectTo: '/main'
+    });
+}])
